985ab1561147e72ee52ac2381eb2e078de51265f
Khorsed-Alam <khorsedalam0472@gmail.com>
2025-11-26 01:29:35 +0600

    01

---
+++ Class_Code/Lab1/01_input_output.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<< "Enter two Number: "<<endl;
+    int a,b;
+    cin>>a>>b;
+    int sum=a+b;
+    cout<<"The sum is: "<<a<<" + "<<b<<" = "<<sum<<endl;
+    return 0;
+}
